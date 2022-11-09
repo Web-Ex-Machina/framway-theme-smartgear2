638284ef9beeb86ef5d9df4a68093bb898dcf6d8
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-11-09 18:37:55 +0100

    add container config override

--- config.js
+++ config.js
@@ -2,4 +2,21 @@ module.exports = {
 	'spacing': '2em',
 	'primary': 'colors(blue)',
 	'secondary': 'colors(blacklight)',
+
+	// 'breakpoints': {
+	// 	'xl' 	: '1400px',
+	// 	'lg' 	: '1200px',
+	// 	'md' 	: '992px',
+	// 	'sm' 	: '768px',
+	// 	'xs' 	: '576px',
+	// 	'xxs'   : '400px',
+	// },
+	'container':{
+		'xl'  : '1140px', // 1320px
+		'lg'  : '960px', // 1140px
+		// 'md'  : '960px',
+		// 'sm'  : '720px',
+		// 'xs'  : '540px',
+		// 'xxs' : '100%',
+	},
 };
