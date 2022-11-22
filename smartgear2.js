a016c1ec3fd6de7ce8ebc51415c48f914d8d017c
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-11-22 11:37:39 +0100

    [bug] remove forgotten test on lightbox links

--- smartgear2.js
+++ smartgear2.js
@@ -60,7 +60,6 @@ $(function () {
     $('a[data-lightbox]').each(function(){
         var item = $(this);
         $(this).attr('data-modal',$(this).attr('data-lightbox'));
-        $(this).attr('data-autoload',false);
         if ($('a[data-lightbox='+item.attr('data-lightbox')+']').length > 1) {
             $('a[data-lightbox='+item.attr('data-lightbox')+']').each(function(i){
                 $(this).attr('data-gallery',$(this).attr('data-lightbox'));
